a3ba92b95c46de105e5535ba6426a9f2a25cd4cd
Stilyan-Kangalov <stilyan_kangalov@abv.bg>
2016-09-02 00:06:29 +0300

    Automated commit by Speedy_Git.bat on Fri 09/02/2016 12:06 AM

---
+++ snipenator.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+echo ##############################################################################
+echo                           ##### Speedy Git #####
+echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+color F
+REM get date and time
+for /f "delims=" %%a in ('date/t') do @set mydate=%%a
+for /f "delims=" %%a in ('time/t') do @set mytime=%%a
+set currentTime=%mydate%%mytime%
+call git status
+echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+echo.
+REM add all new files with auto-commit
+call git add .
+call git commit -a -m "Automated commit by Speedy_Git.bat on %currentTime%"
+call git push origin master
+echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+echo.
+echo ### Great, everything went well! Press any key for exit.
+pause >nul
