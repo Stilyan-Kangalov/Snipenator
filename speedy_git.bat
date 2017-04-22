0ca9101565f6930573590055ee1c32110c0c3380
Stilyan-Kangalov <stilyan_kangalov@abv.bg>
2017-04-22 04:29:58 +0300

    Automated commit by Speedy_Git on 22/04/2017 04:29

--- speedy_git.bat
+++ speedy_git.bat
@@ -1,17 +1,10 @@
 @ECHO OFF
 
-echo ##############################################################################
-echo                           ##### Speedy Git #####
-echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 for /f "delims=" %%a in ('date/t') do @set mydate=%%a
 for /f "delims=" %%a in ('time/t') do @set mytime=%%a
 set currentTime=%mydate%%mytime%
-echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-echo.
 REM add all new files with auto-commit
 call git add .
 call git commit -a -m "Automated commit by Speedy_Git on %currentTime%"
 call git push origin master
-echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-echo.
 exit
