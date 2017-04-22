3928af5712687ac827dc916850f161d09d64637c
Stilyan-Kangalov <stilyan_kangalov@abv.bg>
2017-04-22 04:26:02 +0300

    Automated commit by Speedy_Git on 22/04/2017 04:26

--- speedy_git.bat
+++ speedy_git.bat
@@ -3,12 +3,9 @@
 echo ##############################################################################
 echo                           ##### Speedy Git #####
 echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-color F
-REM get date and time
 for /f "delims=" %%a in ('date/t') do @set mydate=%%a
 for /f "delims=" %%a in ('time/t') do @set mytime=%%a
 set currentTime=%mydate%%mytime%
-call git status 
 echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 echo.
 REM add all new files with auto-commit
@@ -17,5 +14,4 @@ call git commit -a -m "Automated commit by Speedy_Git on %currentTime%"
 call git push origin master
 echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 echo.
-echo ### Great, everything went well!
 exit
