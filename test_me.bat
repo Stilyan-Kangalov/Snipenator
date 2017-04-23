e5dbed0b941a657a2eb0b4fb7a5bf7bcb5fdc6e6
Stilyan-Kangalov <stilyan_kangalov@abv.bg>
2017-04-23 06:55:42 +0300

    Automated commit by Speedy_Git on

--- test_me.bat
+++ test_me.bat
@@ -1,7 +1,16 @@
 @echo off
 
 FOR /F "tokens=* USEBACKQ" %%F IN (`call git status`) DO (
-SET var=%%F
+IF NOT "%%F" == "nothing to commit, working directory clean" DO (
+REM get current time and set it up
+for /f "delims=" %%a in ('date/t') do @set mydate=%%a
+for /f "delims=" %%a in ('time/t') do @set mytime=%%a
+set currentTime=%mydate%%mytime%
+REM add all new files with auto-commit
+call git add .
+call git commit -a -m "Automated commit by Speedy_Git on %currentTime%"
+call git push origin master
+)
 )
 ECHO %var%
 
