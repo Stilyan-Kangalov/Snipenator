ac87c78a9c40b26f681661a9bc33c1eb22b064be
Stilyan-Kangalov <stilyan_kangalov@abv.bg>
2017-04-23 20:28:35 +0300

    Automated commit by Speedy_Git on 23/04/2017 20:28

--- ShadowGit.ps1
+++ ShadowGit.ps1
@@ -13,15 +13,15 @@
               } 	
 
 ### DEFINE ACTIONS AFTER AN EVENT IS DETECTED
-    $action2 = { & "G:\Side_Project\Snipenator\Speedy_Git.bat" Write-Host }
+    $action2 = { & "G:\Side_Project\Snipenator\Speedy_Git.bat" }
 	
 	$action3 = { cmd.exe /c 'git add .'  } 
 	
-###	$action4 = Start-Process -FilePath "G:\Side_Project\Snipenator\Speedy_Git.bat";
+	$action4 = { Start-Process -FilePath "G:\Side_Project\Snipenator\Speedy_Git.bat" }
 	
 ### DECIDE WHICH EVENTS SHOULD BE WATCHED
-    Register-ObjectEvent $watcher "Created" -Action $action2 
-    Register-ObjectEvent $watcher "Changed" -Action $action2
-    Register-ObjectEvent $watcher "Deleted" -Action $action2
-    Register-ObjectEvent $watcher "Renamed" -Action $action2
+    Register-ObjectEvent $watcher "Created" -Action $action4
+    Register-ObjectEvent $watcher "Changed" -Action $action4
+    Register-ObjectEvent $watcher "Deleted" -Action $action4
+    Register-ObjectEvent $watcher "Renamed" -Action $action4
 	while ($true) {sleep 5}
