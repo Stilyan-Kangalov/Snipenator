455b7df32c00a9bc34df6bf9ca3a8a6af3f27353
Stilyan-Kangalov <stilyan_kangalov@abv.bg>
2017-04-24 02:12:05 +0300

    Automated commit by Speedy_Git on 24/04/2017 02:12

--- monitor.rb
+++ monitor.rb
@@ -5,6 +5,7 @@ print "Lets Start Monitoring"
 FileWatcher.new(["G:/Side_Project/Snipenator"]).watch() do |filename, event|
   if(event == :changed)
     puts "File updated: " + filename
+	system "start G:\\Side_Project\\Snipenator\\test_me.bat"
   end
   if(event == :delete or event == :new)
     puts "File deleted: " + filename
