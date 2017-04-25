eacc350a755cfe732ee8231c8bedfc78ac463460
Stilyan-Kangalov <stilyan_kangalov@abv.bg>
2017-04-25 22:55:00 +0300

    Automated commit by Speedy_Git on 25/04/2017 22:54

--- monitor.rb
+++ monitor.rb
@@ -13,11 +13,13 @@ FileWatcher.new([$dir]).watch() do |filename, event|
   if(event == :changed)
     puts "File updated: " + filename
 	puts "#####"
+	puts
 	system "start " + $dir + "/Speedy_Git.bat"
   end
   if(event == :delete)
     puts "New File Action: " + filename
 	puts "#####"
+	puts
 	system "start " + $dir + "/Speedy_Git.bat"
   end
 end
